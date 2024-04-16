1b0bf066b650af085595d2ae7bebbd19fe9d8c91
albertNSBM <albertnsaba2018@gmail.com>
2024-04-16 15:39:10 +0200

    login_finished

--- admin_reg/lib/main.dart
+++ admin_reg/lib/main.dart
@@ -1,13 +1,16 @@
 
 import 'package:flutter/material.dart';
 import 'package:admin_reg/pages/home.dart';
+import 'package:admin_reg/pages/login.dart';
+
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: '/home',
+    initialRoute: '/login',
     routes: {
       '/home':(context)=>Home(),
+      '/login':(context)=>Login(),
 
     },
   ));
---
+++ admin_reg/lib/pages/login.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(image: AssetImage('assets/Reg.png'),)
+            ],
+          ),
+          SizedBox(height: 20,),
+          Column(
+            children: [
+              Container(height: 5,width: 600,
+              decoration: BoxDecoration(color: Colors.red,borderRadius: BorderRadius.circular(10)),),
+              Container(width: 600,height: 400,decoration: BoxDecoration(color: Colors.grey),
+              child: Column(children: [
+                SizedBox(height: 30,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                  Text('Admin Login',style: TextStyle(fontWeight: FontWeight.bold),)
+                ],),
+                SizedBox(height: 30,),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                
+                    SizedBox(height: 10,),
+                    Container(
+                      height: 30,
+                      width: 300,
+                      child: TextFormField(
+                              decoration: InputDecoration(
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.redAccent),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.redAccent),
+                                  ),
+                                  prefixIcon: Icon(Icons.email,size: 10,),
+                                  labelText: 'Email ',
+                                  labelStyle: TextStyle(fontSize: 15.0)),
+                              // validator: (value){
+                              //   if (value == null || value.isEmpty) {
+                              //     return "*";
+                              //   }
+                              // },
+                            )),
+                            SizedBox(height: 40,),
+                          Container(
+                            height: 30,
+                            width: 300,
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.redAccent),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.redAccent),
+                                  ),
+                                  prefixIcon: Icon(Icons.lock,size: 10,),
+                                  labelText: 'Password ',
+                                  labelStyle: TextStyle(fontSize: 15.0)),
+                              // validator: (value){
+                              //   if (value == null || value.isEmpty) {
+                              //     return "*";
+                              //   }
+                              // },
+                            )),
+
+            
+
+                  ],
+                ),
+                SizedBox(height: 60,),
+               ElevatedButton(
+                        onPressed: () {
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          minimumSize: Size(200, 40),
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(2))),
+                        ),
+                        child: Text(
+                          'Login',
+                          style: TextStyle(color: Colors.black),
+                        )),
+              ],),)
+            ],
+          )
+        ],),
+    );
+  }
+}
